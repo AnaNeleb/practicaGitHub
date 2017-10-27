beea0175c7c1013963fe524a1a8553378db14072
DiegoP <diego_sc25@outlook.com>
2017-10-26 22:07:30 -0500

    Inserción de footer a la página
    Se añadió el copyright en el footer de la página y un cambio minimo en el inputtext para resolver un comflicto.

--- EjercicioA/view/crear.php
+++ EjercicioA/view/crear.php
@@ -14,10 +14,14 @@ and open the template in the editor.
             <input type="hidden" value="guardar" name="opcion">
             Codigo:<input type="text" name="codigo"><br>
             Titulo:<input type="text" name="titulo"><br>
-            Anio:<input type="text" name="anio"><br>
+            Anio:<input type="text" name="año"><br>
             Autor:<input type="text" name="autor"><br>
             Paginas:<input type="text" name="paginas"><br>
             <input type="submit" value="Crear">
         </form>
+        <footer>
+            <p>Esta Página fue creada para el registro de libros de la Biblioteca Nacional, está bajo
+            copyright de Universidad UTN  ©2017</p>
+        </footer>
     </body>
 </html>
